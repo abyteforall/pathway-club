f4af17f644f2759b657cc5690822f2a894784588
Turtsle <bozorgi@gmail.com>
2025-09-22 18:26:38 -0400

    about us page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,24 @@
-import Navbar from '../components/Navbar';
+import "../styles/About.css";
+import { Link } from "react-router-dom";
 
 function About() {
+  return (
+    <main className="about-page">
+      <div className="about-content">
+        <div className="title-banner about-banner">
+          <span className="title-inner">About Us</span>
+        </div>
+        <div className="about-inner"></div>
+      </div>
+      <div className="about-btn-rotate-wrapper">
+        <div className="button-wrap about-home-button">
+          <Link to="/" className="arrow-nav-button about-home-link">
+            HOME
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default About;
